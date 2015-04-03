b7495abbb8b8a266ef9a0baea300fc6a3786f79d
Marco Solazzi <msolazzi@aquest.it>
2015-04-03 18:11:46 +0200

    npm

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ module.exports = function (remote, files, wok) {
             }).forEach(function (filepath) {
                 remote.copy(filepath, filepath.replace(/^template\//, ''));
             });
-            this.npmInstall(['grunt-contrib-jade'], { 'saveDev': true });
+            wok.npmInstall(['grunt-contrib-jade'], { 'saveDev': true });
             done();
         }
     };
